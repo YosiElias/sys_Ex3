0e0f5322530e56f12f60e51287e49fecbb68b0ae
YosiElias <yosielias1313@gmail.com>
2021-12-19 11:07:08 +0200

    pass all testing

--- main.c
+++ main.c
@@ -2,18 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #define WORD 30
-#define TXT 1024
+#define TXT 1026
 
 void gimatriaFunc();   //A
 void atbashFunc();  //B
-// void atbashFunc(char word, char text);   //C
 void atbashConvert(char* str);  //for B
 
 void printSub(char *text, int word, int i);
 int anagram(int size);
 
 char word[31], text[1026]; //global variable
-char wordAtbash[31]; //textAtbash[258]; //global variable
+char wordAtbash[31]; //global variable
 
 
 
@@ -32,7 +31,6 @@ int printSeqGim(int size) {
     int wordGim = 0;
     for (int i = 0;i< strlen(word);i++)
         wordGim += gimatria(word[i]);
-//    printf("Gematria Sequences: ");
     int count = 0;
     char *f = text, *l = text;
     int a = 0, b = 0;
@@ -103,9 +101,6 @@ int main()
     atbashFunc();
     printf("\nAnagram Sequences: ");
     anagram(strlen(text));
-    // printf("\n");
-    // printf("%s", word);     //Todo: for self testing
-    // printf("\n%s", text);     //Todo: for self testing
     return 0;
 }
 
@@ -113,7 +108,6 @@ int main()
 
 void atbashConvertWord()
 {
-    // char ans[strlen(str)+1];
     int i=0;
     while(word[i]!='\0')
     {
@@ -121,44 +115,29 @@ void atbashConvertWord()
         {
             if(word[i]>='A'&&word[i]<='Z')
                 wordAtbash[i] = 'Z'+'A'-word[i];
-            // printf("%c",'Z'+'A'-str[i]);
             if(word[i]>='a'&&word[i]<='z')
                 wordAtbash[i] = 'z'+'a'-word[i];
-            // printf("%c",'z'+'a'-str[i]);
         }
 
         if(((word[i]>=0&&word[i]<65)||(word[i]>90&&word[i]<97)||(word[i]>122&&word[i]<=127)))
         {
             wordAtbash[i] = word[i];
-            // printf("%c",str[i]);
         }
 
         i++;
     }
-    // printf("%s", wordAtbash);
-    // return ans;
 }
 
 
 void atbashFunc()
 {
     atbashConvertWord();
-//    atbashConvertText();
-//    char *temp = malloc(strlen(text));
-//    char* sWord = &wordAtbash;
     char* sTempText = text;
     char* sTempRevText = text;
-//    char* sRevWord = &wordAtbash[strlen(wordAtbash)-1];
-    // char* curWord = &wordAtbash;
-//    char* curRevWord = &wordAtbash[strlen(wordAtbash)-1];
-    // char *curText = textAtbash[0];
-    // char ans[258], ansRev[258];
     int lenghText = strlen(text), lenghWord =strlen(wordAtbash),
             countWord = 0, countRevWord = 0,
             curText=0, curWord=0, curRevWord=lenghWord-1,
             firstTime=1;
-//    printf("\n----%s----", textAtbash);
-
     for (int i=0; i<lenghText; i++)
     {
         if (text[curText]==' ' || text[curText]=='\t' || text[curText]=='\n')
@@ -178,7 +157,6 @@ void atbashFunc()
                 curWord++;
                 countWord++;
                 if (curWord == lenghWord) {
-//                    printf("\nans:");
                     printSub(sTempText, countWord, firstTime);
                     firstTime=0;
                     countWord = 0;
@@ -194,7 +172,6 @@ void atbashFunc()
                     countWord=1;
                     sTempText = &text[curText];//init new start to word in text
                     if (curWord == lenghWord) {
-//                        printf("\nans:");
                         printSub(sTempText, countWord, firstTime);
                         firstTime=0;
                         countWord = 0;
@@ -212,7 +189,6 @@ void atbashFunc()
                 curRevWord--;
                 countRevWord++;
                 if (curRevWord == -1) {
-//                    printf("\nans REV:");
                     printSub(sTempRevText, countRevWord, firstTime);
                     firstTime=0;
                     countRevWord = 0;
@@ -227,7 +203,6 @@ void atbashFunc()
                     countRevWord=1;
                     sTempRevText = &text[curText];//init new start to word in text
                     if (curRevWord == -1) {
-//                        printf("\nans REV:");
                         printSub(sTempRevText, countRevWord, firstTime);
                         firstTime=0;
                         countRevWord = 0;
@@ -260,7 +235,6 @@ void printSub(char *text, int word, int first) {
 
 int cmp(const void *a, const void *b) { return *(char *)a - *(char *)b; }
 int anagram(int size) {
-//    printf("Anagram Sequences: ");
     int a = 0, b = 0, count = 0, flag = 0;
     int length = strlen(word);
     for (int i = 0; i < length; i++) {
--- makefile
+++ makefile
@@ -3,57 +3,17 @@ CFLAGS = -Wall -g
 AR=ar
 OBJECTS_MAIN=main.o
 OBJECT = advancedClassificationLoop.o basicClassification.o 
-# C2COMPILE = func.c
 C2COMPILE = main.c
 #make all
 all: stringProg 
 
-#make main:
+#make stringProg:
 main.o: $(C2COMPILE) 
 	$(CC) $(CFLAGS) -c $(C2COMPILE) -o main.o
 stringProg: main.o
 	$(CC) $(CFLAGS) -o stringProg main.o
 
-#make connections:
-connections: main.o libmath.so
-	$(CC) $(CFLAGS) -o connections main.o ./libmath.so -lm  
 
-libmath.so: getArray.o my_mat.h
-	$(CC) -shared -o libmath.so getArray.o
-
-getArray.o: getArray.c my_mat.h
-	$(CC) $(CFLAGS) -c getArray.c -lm
-
-
-
-#make maindloop:
-maindloop: main.o libclassloops.so
-	$(CC) $(CFLAGS) -o maindloop main.o ./libclassloops.so -lm
-#make maindrec:
-maindrec: main.o libclassrec.so
-	$(CC) $(CFLAGS) -o maindrec main.o ./libclassrec.so -lm
-#make loops:
-advancedClassificationLoop.o: advancedClassificationLoop.c NumClass.h
-	$(CC) $(CFLAGS) -c advancedClassificationLoop.c -lm
-loops:libclassloops.a
-libclassloops.a: $(OBJECTS_LOOP)
-	$(AR) -rcs libclassloops.a $(OBJECTS_LOOP)
-#make recursives:
-basicClassification.o: basicClassification.c NumClass.h
-	$(CC) $(CFLAGS) -c basicClassification.c -lm
-advancedClassificationRecursion.o: advancedClassificationRecursion.c NumClass.h
-	$(CC) $(CFLAGS) -c advancedClassificationRecursion.c -lm
-recursives:libclassrec.a
-libclassrec.a: $(OBJECTS_REC)
-	$(AR) -rcs libclassrec.a $(OBJECTS_REC)
-#make recursived:
-recursived:libclassrec.so
-libclassrec.so: $(OBJECTS_REC)
-	$(CC) -shared -o libclassrec.so $(OBJECTS_REC)
-#make loopd:
-loopd:libclassloops.so
-libclassloops.so: $(OBJECTS_LOOP) NumClass.h
-	$(CC) -shared -o libclassloops.so $(OBJECTS_LOOP)
 
 .PHONY: clean all
 
